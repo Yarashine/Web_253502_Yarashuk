885e0adba5e6481d5de03449687165305b94773a
PavelYaroshuk <pavel.yaroshuk1981@gmail.com>
2024-09-05 23:30:52 +0300

    Add project files.

---
+++ Web_253502_Yarashuk/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_253502_Yarashuk.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/submit")]
+        public IActionResult SubmitForm(string login, string password)
+        {
+            // Логика обработки данных формы
+            return View();
+        }
+
+    }
+}
---
+++ Web_253502_Yarashuk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_253502_Yarashuk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
